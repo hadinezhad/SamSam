093d388230df52594984de1f9ff0dda82d57f58e
Hadi <hadinezhad@users.noreply.github.com>
2017-07-29 15:35:50 +0430

    farsi-font2

--- manageUser/form.py
+++ manageUser/form.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import User
 
 
 class UserRegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'password']
